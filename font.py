a98ad5966d403083ded1ed7ba7c24e4a03603746
phbice <fpie2_314@163.com>
2022-08-22 23:44:31 +0800

    完善基本类型，读取 offset table 和 所有 table record

--- font.py
+++ font.py
@@ -1,4 +1,5 @@
 import io
+import math
 # import struct
 import enum
 import datetime
@@ -49,32 +50,32 @@ import datetime
 #     Offset16     = (2, '短偏移量，实际是uint16')
 #     Offset32     = (4, '长偏移量，实际是uint32')
 
-class _Type:
+class _Atom:
     '''数据类型的基类'''
     size = None # 数据大小（字节）
     discription = '' # 描述
     @classmethod
     def from_bytes(cls, bytes_):
         '''将字节转化为相应类型的数据，大端序'''
-        return bytes_
+        return cls(bytes_)
     def to_bytes(self):
         '''将相应类型的数据转化为字节，大端序'''
-        return self
+        return bytes(self)
 
-class _Uint(_Type, int):
+class _Uint(_Atom, int):
     '''无符号整数基类'''
     @classmethod
     def from_bytes(cls, bytes_):
-        return int.from_bytes(bytes_, 'big', signed=False)
+        return cls(int.from_bytes(bytes_, 'big', signed=False))
     def to_bytes(self):
         return int.to_bytes(self, self.size, 'big', signed=False)
 
-class _Int(_Type, int):
+class _Int(_Atom, int):
     '''有符号整数基类'''
     @classmethod
     def from_bytes(cls, bytes_):
-        return int.from_bytes(bytes_, 'big', signed=True)
-    def to_bytese(self, bytes_):
+        return cls(int.from_bytes(bytes_, 'big', signed=True))
+    def to_bytes(self):
         return int.to_bytes(self, self.size, 'big', signed=True)
 
 class uint8(_Uint):
@@ -105,9 +106,17 @@ class int32(_Int):
     size = 4
     description = '32位整数'
 
-class Fixed(_Type):
-    '''TODO'''
-    (4, '32位定点数，高16位整数，低16位小数')
+class Fixed(_Atom, float):
+    size = 4
+    description = '32位定点数，高16位整数，低16位小数'
+    @classmethod
+    def from_bytes(cls, bytes_):
+        return (int16.from_bytes(bytes_[:2]) +
+            uint16.from_bytes(bytes_[2:]) / 0x10000)
+    def to_bytes(self):
+        int_part = int16(math.floor(self))
+        dec_part = uint16(round((self - int_part) * 0x10000))
+        return int_part.to_bytes() + dec_part.to_bytes()
 
 class FWORD(int16):
     description = 'int16类型，以字体设计单位计量'
@@ -115,15 +124,19 @@ class FWORD(int16):
 class UFWORD(uint16):
     description = 'uint16类型，以字体设计单位计量'
 
-class F2DOT14(_Type):
+class F2DOT14(_Atom):
     '''TODO'''
     (2, '16位定点数，高2位整数，低14位小数')
 
 class LONGDATETIME(_Int):
     size = 8
     description = '时间日期，用从1904-01-01午夜12:00开始经过的秒数表示，是64位有符号整数'
+    ref_time = datetime.datetime(1904, 1, 2, 0, 0, 0, 0)
+    def __str__(self):
+        return (self.ref_time + datetime.timedelta(seconds=self)
+            ).isoformat(' ','seconds')
 
-class Tag(_Type, bytes):
+class Tag(_Atom, bytes):
     size = 4
     description = '4个uint8组成的数组，用于区分table等对象'
 
@@ -137,7 +150,7 @@ class Offset32(uint32):
 
 
 class _Table(dict):
-    '''Table 接口，用于派生子类
+    '''Table 基类，用于派生子类
     子类需指定类属性 structure
     structure 是由tuple: (type, name, description) 组成的元组
     '''
@@ -166,6 +179,14 @@ class OffsetTable(_Table):
         (uint16, 'rangeShift', 'numTables*16 - searchRange')
     )
 
+class TableRecord(_Table):
+    structure = (
+        (Tag, 'tableTag', 'Table identifier'),
+        (uint32, 'checkSum', 'CheckSum for this table'),
+        (Offset32, 'offset', 'Offset from begining of TrueType font file'),
+        (uint32, 'length', 'Length of this table')
+    )
+
 class Font:
     '''读取字体文件，提供对字体数据的访问方法'''
     def __init__(self, filename):
@@ -174,11 +195,23 @@ class Font:
 
     def parse(self, fontfile:io.BufferedReader):
         self.offset_table = OffsetTable.unpack(fontfile)
+        self.table_records = [TableRecord.unpack(fontfile)
+            for _ in range(self.offset_table['numTables'])]
+
 if __name__ == '__main__':
     # pass
     # for k, v in Type.__members__.items():
     #     print(v, v.size, v.description)
+    # print(Fixed.from_bytes(b'\xff\xf6\xff\xfc'))
+    # print(Fixed(-9.000061).to_bytes())
+    # print(LONGDATETIME(3734255578))
     font = Font('DejaVuSans.ttf')
+    print('-'*15 + 'offset table' + '-'*15)
     for name, value in font.offset_table.items():
-        print(name, value, OffsetTable.descriptions[name],
-            sep='\t')
+        print(f'{name: <14}{str(value): <14}'
+            f'{OffsetTable.descriptions[name]: <14}')
+    for table_record in font.table_records:
+        print('-'*15 + 'table record' + '-'*15)
+        for name, value in table_record.items():
+            print(f'{name: <14}{str(value): <14}'
+                f'{TableRecord.descriptions[name]: <14}')
